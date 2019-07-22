8c6f1c13fb3c8521c3300bf832c47bb230950314
Mikolaj Lukasik <mlukasik@gmail.com>
2019-07-22 22:28:46 +0100

    Initial commit

---
+++ Hobby Tutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hobby Tutorial
+//
+//  Created by Mikolaj Lukasik on 21/07/2019.
+//  Copyright © 2019 Mikolaj Lukasik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
